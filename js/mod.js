ed2eef70dec53730544d346a1487bbadcdcb1013
Emessjay <jackprattporter@gmail.com>
2025-01-07 13:40:24 -0800

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Knowledge Tree",
+	author: "Emessjay",
+	pointsName: "wisdom",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
